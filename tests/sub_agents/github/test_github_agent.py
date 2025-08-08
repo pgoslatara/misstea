249462e4769214838c2847f9d1d3dbb20d91893b
Padraic Slattery <pgoslatara@gmail.com>
2025-08-08 16:07:48 +0200

    Ensure relevant subagent is being called during tests

--- tests/sub_agents/github/test_github_agent.py
+++ tests/sub_agents/github/test_github_agent.py
@@ -3,13 +3,14 @@ from click.testing import CliRunner
 from misstea.main import cli
 
 
-def test_github_agent_e2e():
+def test_github_agent_e2e(caplog):
     """Test that the github agent can be called and returns the expected output."""
     runner = CliRunner()
     result = runner.invoke(
         cli,
         ["run"],
-        input="When was the godatadriven/dbt-bouncer repository created? Return only the date value, do not include any other characters. Use the yyyy-MM-dd format.",
+        input="When was the 'godatadriven/dbt-bouncer' repository created? Return only the date value, do not include any other characters. Use the yyyy-MM-dd format.",
     )
 
+    assert caplog.text.find("name: call_github_agent, args: {'question': ") >= 0
     assert result.stdout.split("\n")[-2].split(":")[-1].strip() == "2024-06-25"
--- tests/sub_agents/terraform/test_terraform_agent.py
+++ tests/sub_agents/terraform/test_terraform_agent.py
@@ -3,13 +3,14 @@ from click.testing import CliRunner
 from misstea.main import cli
 
 
-def test_terraform_agent_e2e():
+def test_terraform_agent_e2e(caplog):
     """Test that the terraform agent can be called and returns the expected output."""
     runner = CliRunner()
     result = runner.invoke(
         cli,
         ["run"],
-        input="Using Terraform AWS provider version 6.0.0, how many attributes are exported by the aws_s3_bucket resource? Return the result as an integer, do not include any other information or string characters.",
+        input="Using Terraform AWS provider version 6.0.0, count many attributes are exported by the aws_s3_bucket resource? Return the result as an integer, do not include any other information or string characters.",
     )
 
+    assert caplog.text.find("name: call_terraform_agent, args: {'question': ") >= 0
     assert int(result.stdout.split("\n")[-2].split(":")[-1].strip()) == 9
