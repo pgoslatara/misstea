90a59f82270b83f945f1b7b665701fdf09a24183
Padraic Slattery <pgoslatara@gmail.com>
2025-08-19 12:56:35 +0100

    More evals on coding agent

--- tests/sub_agents/coding/test_coding_agent.py
+++ tests/sub_agents/coding/test_coding_agent.py
@@ -1,12 +1,28 @@
+from pathlib import Path
+
 import pytest
 from google.adk.evaluation.agent_evaluator import AgentEvaluator
 
 
-@pytest.mark.flaky(reruns=3)
+# @pytest.mark.flaky(reruns=3)
 @pytest.mark.asyncio
 async def test_coding_agent_with_single_test_file():
     """Test the coding agent's basic ability via a session file."""
-    await AgentEvaluator.evaluate(
-        agent_module="misstea.sub_agents.coding",
-        eval_dataset_file_path_or_dir="tests/sub_agents/coding/coding_agent.test.json",
-    )
+
+    def cleanup() -> None:
+        """Delete files used for testing if they exist."""
+        Path("./tests/sub_agents/coding/dummy_file_write.txt").unlink(missing_ok=True)
+        Path("./tests/sub_agents/coding/dummy_file_replace.txt").unlink(missing_ok=True)
+
+    try:
+        cleanup()
+
+        with Path("./tests/sub_agents/coding/dummy_file_replace.txt").open("w") as f:
+            f.write("This is a string.")
+
+        await AgentEvaluator.evaluate(
+            agent_module="misstea.sub_agents.coding",
+            eval_dataset_file_path_or_dir="tests/sub_agents/coding/coding_agent.test.json",
+        )
+    finally:
+        cleanup()
